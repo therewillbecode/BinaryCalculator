570c65eadf1c070b5e7dab1f99f8bb05463882e9
Tom <tomw08@gmail.com>
2015-10-29 14:22:34 +0000

    removed unnecessary comments and whitespace

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 module.exports.conv = to_decimal;
 module.exports.calculate = calculate;
 
-
 function calculate(a, b){
-
+    
 return to_decimal(a) + to_decimal(b)
-
 }
-
 
 function to_decimal(x){
     var cur = Math.pow(2, (x.length));
 
     return Array.prototype.map.call(x, function(val, index){
         cur /= 2;
-
-     //   console.log('index:' + (x.length-index) + ' val : ' + (Math.pow(index, 2)));
         if (val == 1 && index == x.length -1){
-            return (1);
+            return 1;
         }
         if (val == 1 && index == x.length -2) {
             cur =2;
-            return (2);
+            return 2;
         }
         if (val == 1){
             pos = x.length - (index + 1);
-          //  console.log(pos);
-            // return counter;
-            //'' recursively  double prev val
-            return (cur);
+            return cur;
         }
         else{
             return 0;
